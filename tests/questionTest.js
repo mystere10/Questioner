ad1b5aba98ca448e5a9741b1968be0416dc92d73
Nkunzi <nkunziinnocent@gmail.com>
2019-01-15 12:18:14 +0200

    [Chore #163219253] fixing bugs into test files

--- tests/questionTest.js
+++ tests/questionTest.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-undef */
 import chai from 'chai';
 import http from 'chai-http';
 import index from '../src/index';
@@ -7,56 +8,56 @@ const should = chai.should();
 chai.use(http);
 
 describe('Question tests', () => {
-    let id;
-    it('should create a question', (done) => {
-        const newQuestion = {
-            createdBy: 'f98ef240-0aa4-11e9-9315-0fcfc3931fe8',
-            meetup: '356083b0-0aa5-11e9-9315-0fcfc3931fe8',
-            title: 'EDUCATION',
-            body: 'The quality of education...',
-            votes: 4
-        };
-        chai.request(index)
-        .post('/api/v1/questions/')
-        .send(newQuestion)
-        .end((err, res) => {
-            res.should.have.status(201);
-            res.should.be.json;
-            res.body.should.be.a('object');
-            res.body.question.should.have.property('createdBy');
-            res.body.question.should.have.property('meetup');
-            res.body.question.should.have.property('title');
-            res.body.question.should.have.property('body');
-            id = res.body.question.id;
-            done();
-        });
-    });
+  let id;
+  it('should create a question', (done) => {
+    const newQuestion = {
+      createdBy: 'f98ef240-0aa4-11e9-9315-0fcfc3931fe8',
+      meetup: '356083b0-0aa5-11e9-9315-0fcfc3931fe8',
+      title: 'EDUCATION',
+      body: 'The quality of education...',
+      votes: 4,
+    };
+    chai.request(index)
+      .post('/api/v1/questions/')
+      .send(newQuestion)
+      .end((err, res) => {
+        res.should.have.status(201);
+        res.should.be.json;
+        res.body.should.be.a('object');
+        res.body.question.should.have.property('createdBy');
+        res.body.question.should.have.property('meetup');
+        res.body.question.should.have.property('title');
+        res.body.question.should.have.property('body');
+        id = res.body.question.id;
+        done();
+      });
+  });
 
-    it('should upvote a question', (done) => {
-        chai.request(index)
-        .patch(`/api/v1/questions/${id}`)
-        .end((err, res) => {
-            res.should.have.status(200);
-            res.should.be.json;
-            res.body.question.should.have.property('createdBy');
-            res.body.question.should.have.property('meetup');
-            res.body.question.should.have.property('body');
-            res.body.question.should.have.property('votes').eql(5);
-            done();
-        });
-    });
+  it('should upvote a question', (done) => {
+    chai.request(index)
+      .patch(`/api/v1/questions/${id}/upvote`)
+      .end((err, res) => {
+        res.should.have.status(200);
+        res.should.be.json;
+        res.body.question.should.have.property('createdBy');
+        res.body.question.should.have.property('meetup');
+        res.body.question.should.have.property('body');
+        res.body.question.should.have.property('votes').eql(5);
+        done();
+      });
+  });
 
-    it('should downvote a question', (done) => {
-        chai.request(index)
-        .patch(`/api/v1/questions/downvote/${id}`)
-        .end((err, res) => {
-            res.should.have.status(200);
-            res.should.be.json;
-            res.body.question.should.have.property('createdBy');
-            res.body.question.should.have.property('meetup');
-            res.body.question.should.have.property('body');
-            res.body.question.should.have.property('votes').eql(4);
-            done();
-        });
-    });
+  it('should downvote a question', (done) => {
+    chai.request(index)
+      .patch(`/api/v1/questions/${id}/downvote`)
+      .end((err, res) => {
+        res.should.have.status(200);
+        res.should.be.json;
+        res.body.question.should.have.property('createdBy');
+        res.body.question.should.have.property('meetup');
+        res.body.question.should.have.property('body');
+        res.body.question.should.have.property('votes').eql(4);
+        done();
+      });
+  });
 });
