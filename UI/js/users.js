0eb78dd254754b19b5209d75712977e65978078d
Nkunzi Innocent <innocentnkunzi@yahoo.com>
2019-03-02 20:26:36 +0200

    [Feature #164253504] connecting signup page

--- UI/js/users.js
+++ UI/js/users.js
@@ -1,33 +1,74 @@
+/* eslint-disable linebreak-style */
 const headers = new Headers();
 headers.append('Accept', 'application/json');
 headers.append('Content-type', 'application/json');
-function login () {
-    console.log('login is there!');
-    const formcontent = document.getElementById('loginForm');
-    formcontent.addEventListener('submit', (event) =>{
-        event.preventDefault();
-        
-        const { email, password } = formcontent;
-        const userInfo = { email: email.value, password: password.value };
-        
-        const submitInfo = {
-            method: 'POST',
-            headers: headers,
-            body: JSON.stringify(userInfo),
-        };
-        fetch ('../../api/v1/auth/login', submitInfo)
-        .then(response => response.json())
-        .then((returnUser) => {
-            if (returnUser.token){
-                window.localStorage.setItem('userInfo', JSON.stringify(returnUser));
-                window.location.replace('meetups.html');
-            }else if(returnUser.error){
-                document.getElementById('login-error').innerHTML = returnUser.error;
-                ;
-                // alert(JSON.stringify(returnUser));
-            }else if(returnUser.message){
-                document.getElementById('login-error').innerHTML = returnUser.message;
-            }
-        });
-    });
+function login() {
+  const formcontent = document.getElementById('loginForm');
+  formcontent.addEventListener('submit', (event) => {
+    event.preventDefault();
+    const { email, password } = formcontent;
+    const userInfo = { email: email.value, password: password.value };
+
+    const submitInfo = {
+      method: 'POST',
+      headers,
+      body: JSON.stringify(userInfo),
+    };
+    fetch('../../api/v1/auth/login', submitInfo)
+      .then(response => response.json())
+      .then((returnUser) => {
+        if (returnUser.token && returnUser.user.isadmin) {
+          window.localStorage.setItem('AdminInfo', JSON.stringify(returnUser));
+          window.location.replace('admin.html');
+        } else if (returnUser.token && !returnUser.user.isadmin) {
+          window.localStorage.setItem('userInfo', JSON.stringify(returnUser));
+          window.location.replace('userhome.html');
+        } else if (returnUser.error) {
+          document.getElementById('login-error').innerHTML = returnUser.error;
+
+          // alert(JSON.stringify(returnUser));
+        } else if (returnUser.message) {
+          document.getElementById('login-error').innerHTML = returnUser.message;
+
+          // alert(JSON.stringify(returnUser));
+        }
+      });
+  });
+}
+
+function registration() {
+  const signupform = document.getElementById('signup');
+  signupform.addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    const {
+      firstname, lastname, othername, email, phoneNumber, username, password,
+    } = signupform;
+    const signupData = {
+      firstname: firstname.value,
+      lastname: lastname.value,
+      othername: othername.value,
+      email: email.value,
+      phoneNumber: phoneNumber.value,
+      username: username.value,
+      password: password.value,
+    };
+
+    const submitRegistration = {
+      method: 'POST',
+      headers,
+      body: JSON.stringify(signupData),
+    };
+    fetch('../../api/v1/auth/signup', submitRegistration)
+      .then(response => response.json())
+      .then((registereduser) => {
+        if (registereduser.token && registereduser.user) {
+          window.localStorage.setItem('registeredUser', JSON.stringify(registereduser));
+          document.getElementById('registered').innerHTML = registereduser.message;
+          //   window.location.replace('login.html');
+        } else if (registereduser.error) {
+          document.getElementById('registration-error').innerHTML = registereduser.error;
+        }
+      });
+  });
 }
--- src/controllers/users.js
+++ src/controllers/users.js
@@ -3,6 +3,7 @@
 import Joi from 'joi';
 import bcrypt from 'bcryptjs';
 import jwt from 'jsonwebtoken';
+import { runInNewContext } from 'vm';
 import User from '../model/User';
 import validation from '../helpers/validations';
 import db from '../db/connect';
@@ -21,24 +22,46 @@ const Users = {
     if (error) {
       res.status(400).json({ error: error.details[0].message });
     } else {
-      const user = new User(firstname, lastname, othername, email, phoneNumber, username, password);
-      const query = db(queries.registrations, [user.firstname, user.lastname, user.othername, user.email, user.phoneNumber, user.username, user.password]);
-      query.then((response) => {
-        jwt.sign({ response: response[0] }, 'secretkey', (err, token) => {
-          const {
-            firstname, lastname, othername, email, phoneNumber, username,
-          } = response[0];
-          res.status(201).json({
-            status: '201',
-            token,
-            message: 'User sucessufully registered',
-            user: {
-              firstname, lastname, othername, email, phoneNumber, username,
-            },
+      const checkuseremail = db(queries.useremail, [email]);
+      const checkuserphone = db(queries.userphone, [phoneNumber]);
+      checkuseremail.then((useresponse) => {
+        if (useresponse.length > 0) {
+          res.status(409).json({
+            error: 'Email exists',
           });
-        });
+        } else if (useresponse) {
+          checkuserphone.then((userresponse) => {
+            if (userresponse.length > 0) {
+              res.status(409).json({
+                error: 'Phone number exists',
+              });
+            } else {
+              const user = new User(firstname, lastname, othername, email, phoneNumber, username, password);
+              const query = db(queries.registrations, [user.firstname, user.lastname, user.othername, user.email, user.phoneNumber, user.username, user.password]);
+              query.then((response) => {
+                jwt.sign({ response: response[0] }, 'secretkey', (err, token) => {
+                  const {
+                    firstname, lastname, othername, email, phoneNumber, username,
+                  } = response[0];
+                  res.status(201).json({
+                    status: '201',
+                    token,
+                    message: 'User sucessufully registered',
+                    user: {
+                      firstname, lastname, othername, email, phoneNumber, username,
+                    },
+                  });
+                });
+              }).catch((error) => {
+                res.status(500).send({ error });
+              });
+            }
+          }).catch((error) => {
+            console.log(error);
+          });
+        }
       }).catch((error) => {
-        res.status(500).send({ message: 'Not registered', error });
+        console.log(error);
       });
     }
   },
@@ -62,14 +85,14 @@ const Users = {
         }
         jwt.sign({ response: response[0] }, 'secretkey', { expiresIn: '1h' }, (err, token) => {
           const {
-            firstname, lastname, othername, email, phoneNumber, username,
+            firstname, lastname, othername, email, phoneNumber, username, isadmin,
           } = response[0];
           res.status(200).json({
             status: '200',
             message: 'Welcome',
             token,
             user: {
-              firstname, lastname, othername, email, phoneNumber, username,
+              firstname, lastname, othername, email, phoneNumber, username, isadmin,
             },
           });
         });
